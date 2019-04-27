19ca5c7c6969c21a592a4b8f35079ceee942a85a
Arthur Dooner <shunderpooch@gmail.com>
2019-04-27 17:20:48 -0400

    Changes to the map

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ const config = {
     update: update
   },
   physics: { 
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 500 }, // will affect our player sprite
-      debug: false // change if you need
-    }
+    default: 'arcade' //,
+  //   arcade: {
+  //     gravity: { y: 500 }, // will affect our player sprite
+  //     debug: false // change if you need
+  //   }
   }
 };
 
@@ -58,36 +58,43 @@ function create() {
   enemy.depth = 100000;
 }
 
+/**
+ * Function that returns all the attributes about the map that you could possibly need
+ */
+function getMapInfo() {
+  var data = scene.cache.json.get("map");
+  var mapData = {
+    tileWidth : data.tilewidth,
+    tileHeight : data.tileheight,
+    layer : data.layers[0].data,
+    mapWidth : data.layers[0].width, // Width of the entire map
+    mapHeight : data.layers[0].height, // Height of the entire map
+    centerX : data.layers[0].width * data.tilewidth / 2,
+    centerY : 16,
+
+  }
+  return mapData
+}
+
 function buildMap() {
   //  Parse the data out of the map
-  var data = scene.cache.json.get("map");
-
-  var tilewidth = data.tilewidth;
-  var tileheight = data.tileheight;
-
-  tileWidthHalf = tilewidth / 2;
-  tileHeightHalf = tileheight / 2;
-
-  var layer = data.layers[0].data;
-
-  var mapwidth = data.layers[0].width;
-  var mapheight = data.layers[0].height;
-
-  var centerX = mapwidth * tileWidthHalf;
-  var centerY = 16;
+  var mapData = getMapInfo()
+  //console.log(mapData)
+  tileWidthHalf = mapData.tileWidth / 2;
+  tileHeightHalf = mapData.tileHeight / 2;
 
   var i = 0;
 
-  for (var y = 0; y < mapheight; y++) {
-    for (var x = 0; x < mapwidth; x++) {
-      var id = layer[i] - 1;
+  for (var y = 0; y < mapData.mapHeight; y++) {
+    for (var x = 0; x < mapData.mapWidth; x++) {
+      var id = mapData.layer[i] - 1;
 
       var tx = (x - y) * tileWidthHalf;
       var ty = (x + y) * tileHeightHalf;
 
-      var tile = scene.add.image(centerX + tx, centerY + ty, "tiles", id);
+      var tile = scene.add.image(mapData.centerX + tx, mapData.centerY + ty, "tiles", id);
 
-      tile.depth = centerY + ty;
+      tile.depth = mapData.centerY + ty;
 
       i++;
     }
@@ -96,7 +103,7 @@ function buildMap() {
 
 function update() {
   // return;
-
+  enemy.setVelocity(0)
   if (d) {
     this.cameras.main.scrollX -= 0.5;
 
@@ -110,4 +117,15 @@ function update() {
       d = 1;
     }
   }
+  var enemyX = -100
+  
+  if (enemy.x < 100) {
+    enemyX = 100
+  }
+  if (enemy.x > 1000) {
+    enemyX = -100
+  }
+  enemy.setAngle(-90).setVelocityX(-200);
+  enemy.setVelocityY(-100);
+
 }
